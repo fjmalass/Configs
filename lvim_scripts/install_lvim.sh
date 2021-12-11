8e0ab61d09e8c4d445af2999bf0bcf57190a01bd
Francois Malassenet <malassenet@gmail.com>
2021-12-11 19:30:26 +0100

    lvim_scripts: remove old config copy

--- lvim_scripts/install_lvim.sh
+++ lvim_scripts/install_lvim.sh
@@ -9,6 +9,6 @@ echo "##################"
 echo "copy Configuration without the .git files"
 echo "##################"
 # cp -r ~/Repos/ChristianChiarulli/lvim  ~/.config/
-# cd ~/Repos/ChristianChiarulli/lvim
-# find . \! \( -path "*/.git/*" -or -name ".git" -or -name . -or -name ".gitignore" \) -print | cpio -padvu ~/.config/lvim
+cd ~/Repos/ChristianChiarulli/lvim
+find . \! \( -path "*/.git/*" -or -name ".git" -or -name . -or -name ".gitignore" \) -print | cpio -padvu ~/.config/lvim
 
--- lvim_scripts/lvim_uninstall.sh
+++ lvim_scripts/lvim_uninstall.sh
@@ -7,5 +7,5 @@ rm -rf ~/.local/share/applications/lvim.desktop
 echo "Removing .config/lvim"
 rm -rf ~/.config/lvim/plugin
 # This will remove all the personalized configs (Essential
-echo "copy .config/lvim/config.lua"
-mv -f ~/.config/lvim/config.lua ~/.config/lvim/config_old.lua
+#echo "copy .config/lvim/config.lua"
+# mv -f ~/.config/lvim/config.lua ~/.config/lvim/config_old.lua
